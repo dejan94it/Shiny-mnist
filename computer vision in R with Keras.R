03dcc3b9491797acf11577c99a52e7ec9d9a5d85
dejan94it <dejan94@hotmail.it>
2025-08-05 13:28:28 +0200

    first commit

---
+++ computer vision in R with Keras.R
@@ -0,0 +1,104 @@
+library(keras)
+mnist <- dataset_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+# reshape
+x_train <- array_reshape(x_train, c(nrow(x_train), 784))
+x_test <- array_reshape(x_test, c(nrow(x_test), 784))
+# rescale
+x_train <- x_train / 255
+x_test <- x_test / 255
+
+
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
+
+
+model <- keras_model_sequential()
+model %>% 
+  layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 128, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = 'softmax')
+model
+
+model %>% 
+  compile(
+    loss = 'categorical_crossentropy',
+    optimizer = optimizer_rmsprop(),
+    metrics = c('accuracy')
+  )
+
+history <- model %>% 
+  fit(
+    x_train, y_train, 
+    epochs = 30, batch_size = 128, 
+    validation_split = 0.2
+  )
+
+model %>% evaluate(x_test, y_test)
+
+predictions <- model %>% predict(x_test) %>% k_softmax()
+
+
+
+#CNN
+dim(x_train)
+img_rows <- 28
+img_cols <- 28
+
+x_train <- array_reshape(x_train,
+                         c(nrow(x_train),
+                           img_rows,
+                           img_cols, 1))
+x_test <- array_reshape(x_test,
+                        c(nrow(x_test),
+                          img_rows,
+                          img_cols, 1))
+
+dim(x_train)
+
+x_train <- x_train / 255
+x_test <- x_test / 255
+
+num_classes = 10
+y_train <- to_categorical(y_train, num_classes)
+y_test <- to_categorical(y_test, num_classes)
+
+
+modelconv <- keras_model_sequential()
+modelconv %>% 
+  layer_conv_2d(filters = 8, kernel_size = c(3,3), padding = "same", input_shape = c(28, 28, 1)) %>% 
+  layer_max_pooling_2d() %>% 
+  layer_conv_2d(filters = 4, kernel_size = c(3,3), padding = "same") %>% 
+  layer_max_pooling_2d() %>% 
+  layer_flatten() %>% 
+  layer_dropout(rate = 0.5) %>% 
+  layer_dense(units = 10, activation = "softmax")
+
+modelconv
+
+
+modelconv %>% 
+  compile(
+    loss = 'categorical_crossentropy',
+    optimizer = optimizer_adam(),
+    metrics = c('accuracy')
+  )
+
+
+history <- modelconv %>% 
+  fit(
+    x_train, y_train, 
+    epochs = 10, #batch_size = 128, 
+    validation_split = 0.2
+  )
+
+modelconv %>% evaluate(x_test, y_test)
+ 
+save_model_hdf5(modelconv, "CNNmnist.hdf5")
+ 
---
+++ ui.R
@@ -0,0 +1,63 @@
+
+ui <- fluidPage(
+  theme = bslib::bs_theme(version = 5, bootswatch = "flatly"),  # Optional: modern Bootstrap theme
+  
+  br(),
+  panel(
+    heading = h3("🖊️ Draw a Digit"),
+    status = "primary",
+    solidHeader = TRUE,
+    collapsible = FALSE,
+    
+    fluidRow(
+      column(
+        width = 6,
+        
+        tags$p("Click on the plot to start drawing. Move the mouse to draw, and click again to stop."),
+        
+        plotOutput(
+          "plot",
+          width = "380px",
+          height = "380px",
+          hover = hoverOpts(
+            id = "hover",
+            delay = 100,
+            delayType = "throttle",
+            clip = TRUE,
+            nullOutside = TRUE
+          ),
+          click = "click"
+        ),
+        
+        br(),
+        fluidRow(
+          column(12,
+                 actionBttn("reset", "Reset", style = "material-flat", color = "danger", icon = icon("redo")),
+                 actionBttn("predict", "Predict", style = "material-flat", color = "success", icon = icon("search"))
+          )
+        )
+      ),
+      
+      column(
+        width = 6,
+        
+        h4("Model Output", class = "section-title"),
+        
+        wellPanel(
+          style = "background-color: #f9fcff; border-radius: 10px;",
+          
+          h5("Preprocessed Image:"),
+          plotOutput("digit_image", width = "200px", height = "200px"),
+          
+          br(),
+          
+          h5("Prediction:"),
+          tags$div(
+            style = "font-size:2em; font-weight:bold; color:#2c3e50;",
+            textOutput("prediction")
+          )
+        )
+      )
+    )
+  )
+)
